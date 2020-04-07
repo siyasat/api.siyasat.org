cf5a57cc77afbd3a976ca1c3263750f9b5d790cd
Andresito de Guzman <deguzman.andresito@gmail.com>
2020-04-08 02:12:42 +0800

    add express js

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,8 +1,27 @@
 import * as functions from 'firebase-functions';
+const express = require('express');
+const api = require('./api');
+const v1 = api.v1;
 
-// // Start writing Firebase Functions
-// // https://firebase.google.com/docs/functions/typescript
-//
-// export const helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+const app = express();
+
+// Handle root path
+app.get('/', (req: any, res: any) => {
+    res.json({ message: 'Siyasat API', versions: [{ version: 'v1', status: 'available' }] });
+});
+
+// Handle documentation request
+app.get('/docs', (req:any, res: any) => {
+    res.redirect('https://developer.siyasat.org');
+});
+
+// v1
+app.get('/v1', v1.home.get);
+app.get('/v1/organizations', v1.organizations.get);
+
+// Handle 404
+app.use((req:any, res:any, next:any) => {
+    res.status(404).json({ message: "Endpoint not found or invalid" });
+});
+
+exports.app = functions.https.onRequest(app);
